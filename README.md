b52cfd45ff82f3e0bda22fcf0b4389c6ca113eb1
James Adam Buckland <james.adam.buckland@gmail.com>
2016-12-03 22:29:26 -0600

    final update of text-twist.hs

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # text-twist
+
+```php
+$ runhaskell text-twist.hs
+ ┌ T E X T   T W I S T
+ ├ Please enter a seed word:
+domino
+┏━━━━━━━━━━━━━━━━━━━━━┓
+┃ ~~~ ~~~ ~~~~ ~~~~   ┃
+┃ ~~~ ~~~ ~~~~ domino ┃
+┃ ~~~ ~~~ ~~~~        ┃
+┃ ~~~ ~~~ ~~~~        ┃
+┗┯━━━━━━━━━━━━━━━━━━━━┛
+ ├ Please enter a guess:
+mind
+┏━━━━━━━━━━━━━━━━━━━━━┓
+┃ ~~~ ~~~ ~~~~ ~~~~   ┃
+┃ ~~~ ~~~ mind domino ┃
+┃ ~~~ ~~~ ~~~~        ┃
+┃ ~~~ ~~~ ~~~~        ┃
+┗┯━━━━━━━━━━━━━━━━━━━━┛
+ ├ Please enter a guess:
+mood
+┏━━━━━━━━━━━━━━━━━━━━━┓
+┃ ~~~ ~~~ ~~~~ ~~~~   ┃
+┃ ~~~ ~~~ mind domino ┃
+┃ ~~~ ~~~ ~~~~        ┃
+┃ ~~~ ~~~ mood        ┃
+┗━━━━━━━━━━━━━━━━━━━━━┛
+```
+
+Text Twist is a [classic old internet flash
+game](http://zone.msn.com/gameplayer/gameplayer.aspx?game=texttwist) in which
+the  objective is to, given a starting word, find all the unique words which can 
+be made from its letters.
+
+This is a implementation in Haskell, playable from the shell. I hope the code is
+fairly readable.
+
+## Playing
+You can just play the game:
+```bash
+$ runhaskell text-twist.hs
+```
+or, compile it first if you like:
+```bash
+$ ghc text-twist.hs
+[1 of 1] Compiling Main             ( text-twist.hs, text-twist.o )
+Linking text-twist ...
+
+$ ./text-twist
+ ┌ T E X T   T W I S T
+ │ Please enter a seed word:
+ ...
+```
---
+++ text-twist.hs
@@ -0,0 +1,71 @@
+import Control.Monad
+import Data.Char
+import Data.List.Split
+import Data.Ord
+import Language.Words
+import System.Exit
+import Text.PrettyPrint.Boxes
+import qualified Data.List as L
+import qualified Data.Map as M
+import qualified Data.Set as S
+
+safeWords :: S.Set String
+safeWords = S.filter (\w -> all ($ w) conditions) allWords
+  where allWords = S.fromList allStringWords
+        conditions = [notElem '\233', notElem '\'', all isLower]
+
+getSubWords :: String -> [String]
+getSubWords word = L.nub . L.sortBy (comparing length)
+                 $ concatMap (`ofLength` word) [3..(length word)]
+  where ofLength n = filter (`S.member` safeWords)
+                   . concatMap L.permutations
+                   . filter (\x -> length x == n) . L.subsequences
+                   . map toLower
+
+blankState :: String -> [String] -> M.Map String Bool
+blankState seed = M.fromList . map (\word -> (word, word==seed))
+
+showState :: [String] -> M.Map String Bool -> IO ()
+showState words state = printMatrix state (matrix state)
+  where printMatrix state matrix = do
+          putStrLn $ "┏" ++ replicate n '━' ++ "┓"
+          printBox $ hsep pad left $ map (vcat left . map text)
+                                   $ col ++ matrix ++ col
+          putStrLn $ "┗┯" ++ replicate (pred n) '━' ++ "┛"
+            where n = pad * (1 + length matrix)
+                    + (sum . map (maximum . map length) $ matrix)
+                  col = [replicate (length $ head matrix) ['┃']]
+                  pad = 1
+        matrix = chunksOf sq . map (uncurry render) . L.sortBy keyLen . M.toList
+          where sq = (ceiling $ sqrt $ fromIntegral $ length words)
+                render k v = if v then k else replicate (length k) '~'
+                keyLen (k1, _) (k2, _) = compare (length k1) (length k2)
+
+playGame :: [String] ->  M.Map String Bool -> IO ()
+playGame words state = do
+                         showState words state
+                         when (gameOver) $ die " └ You win!"
+                         tInst "Please enter a guess:"
+                         guess <- getLine
+                         let correct = M.member guess state
+                         let newState = updateState correct guess state
+                         unless correct $ displayIncorrectGuess guess
+                         playGame words newState
+  where gameOver = L.all (==True) $ M.elems state
+        updateState bool = M.update (\_ -> Just bool)
+        displayIncorrectGuess guess
+          | length guess <= 2         = tInst "Too short."
+          | guess `notElem` safeWords = tInst "Not a word."
+          | otherwise                 = tInst "Not a subword."
+
+tInst :: [Char] -> IO ()
+tInst text = putStrLn $ " ├ " ++ text
+
+main = do
+  putStrLn $ " ┌ " ++ "T E X T   T W I S T"
+  tInst "Please enter a seed word:"
+  seed <- getLine
+  when (seed `notElem` safeWords) $ die " └ Use a real word as a seed."
+  let subWords = getSubWords seed
+  when (length subWords <= 2) $ die " └ Not enough subwords."
+  playGame subWords $ blankState seed subWords
